9eaf93d3260b7d8a5de1a622bcdf9394127930d3
Henrique <hb4224984@gmail.com>
2025-06-04 16:33:01 -0300

    mostrando dados

--- src/app/cadelog/page.js
+++ src/app/cadelog/page.js
@@ -9,13 +9,13 @@ export default () => {
         <div className={style.containercad}>
           <h1 className="titulo">Cadastre-se</h1>
             <div className={style.entradas}>
-              <input className={style.input} type="text" placeholder="Nome" required/>
+              <input className={style.input}  type="text" placeholder="Nome" required/>
               <input className={style.input}  type="number" id="cpf" placeholder="CPF" required/>  
               <input className={style.input}  type="date" placeholder="Data de Nascimento" required/> 
               <input className={style.input}  type="text" placeholder="Endereço" required/> 
               <input className={style.input}  type="number" placeholder="Contato" required/>
               <input className={style.input}  type="password" placeholder="Senha" required/>
-              <button className={style.botao} type="button" placeholder="Agendar">Criar</button>
+              <button className={style.botao}  type="button" placeholder="Agendar">Criar</button>
             </div>
         </div>
       </div>
--- src/app/perfil/page.js
+++ src/app/perfil/page.js
@@ -1,17 +1,18 @@
 import style from "./page.module.css";
-import Link from 'next/link'
+import db from "../lib/db"
 
-export default () => {
-  return (
-    <>
+export default async () => {
+    const usuario = await db.query("select * from cliente where id_cliente = '2' ")
+ return (<>
+ {usuario.rows.map( a => (
     <div className={style.geral}>
      <div className={style.container}>
-        <div className={style.cima}><h1>Perfil</h1></div>
+        <div className={style.cima}><h1>{a.nome}</h1></div>
 
         <div className={style.baixo}>
         <div className={style.left}>
             <div className={style.ftdeperfil}></div>
-            <p>Lorem Ipsun Silva</p>
+            <a href={`./perfil/${a.id_cliente}`}><p>Seus Dados</p></a>
         </div>
          <div className={style.rigth}>
             <button className={style.button}>Marcar Consulta</button>
@@ -21,6 +22,9 @@ export default () => {
         </div>
       </div>
     </div>
+    ) 
+  )
+}
     </>
   );
 };
