fd6fc90da8ae3a0e05c664f6b37ea94cf2fff277
Henrique <hb4224984@gmail.com>
2025-07-23 08:32:29 -0300

    pefil

--- src/app/perfil/[id]/ClienteView.js
+++ src/app/perfil/[id]/ClienteView.js
@@ -5,10 +5,13 @@ import Calendario from "../../../components/reactDatePicker";
 
 export default function ClienteView({ cliente }) {
   const [agendarConsulta, setAgendarConsulta] = useState(false);
-
+  const [verProntuario, setVerProntuario] = useState(false);
+  const [editarConsultas, setEditarConsultas] = useState(false);
 
   function fecharModal() {
     setAgendarConsulta(false);
+    setVerProntuario(false);
+    setEditarConsultas(false);
   }
 
   return (
@@ -18,21 +21,19 @@ export default function ClienteView({ cliente }) {
             <div className={style.esquerda}>
             <div className={style.ftdeperfil}></div>
               <h2>{cliente.nome}</h2>
-              <p>{cliente.contato}</p>
               <p>{new Date(cliente.nascimento).toLocaleDateString()}</p> {/*fazer calculo e colocar idade*/}
             </div>
             <div className={style.direita}>
               <h1>Consulta Agendada - xx/xx/xx</h1> {/* colocar consultas aqui */}
-              <button className={style.botao}>Alterar data ou hora</button>
-              <button className={style.botao}>Cancelar</button>
+              <button className={style.botao} onClick={() => setEditarConsultas(true)}>Editar Consultas</button>
             </div>  
         </div>
 
           <div className={style.baixo}>
             <div className={style.esquerda}>
-              <p>xx</p> {/* colocar consultas aqui */}
+              <p>xx</p> {/* colocar consultas realizadas aqui */}
               <p>Consultas Realizadas</p>
-              <button className={style.botao}>Ver Prontuários</button>
+              <button className={style.botao} onClick={() => setVerProntuario(true)}>Ver Prontuários</button>
               <button className={style.botao} onClick={() => setAgendarConsulta(true)}>Nova Consulta</button>
             </div>
             <div className={`${style.direita} ${style.direitaBaixo}`}>
@@ -43,11 +44,31 @@ export default function ClienteView({ cliente }) {
         </div>
       </div>
 
+      {/* modal, janelas de sobreposição */}
+
        {agendarConsulta && (
         <div className={style.modalOverlay} onClick={fecharModal}>
           <div className={style.modal} onClick={e => e.stopPropagation()}>
             <h2>Agendar Consulta</h2>
             <Calendario className={style.datahora} placeholder="Escolher data e hora" mode="schedule" />
+            <button onClick={fecharModal} className={style.fechar}>Fechar</button>
+          </div>
+        </div>
+      )}
+
+      {verProntuario && (
+        <div className={style.modalOverlay} onClick={fecharModal}>
+          <div className={style.modal} onClick={e => e.stopPropagation()}>
+            <h2>Prontuários</h2>
+            <button onClick={fecharModal} className={style.fechar}>Fechar</button>
+          </div>
+        </div>
+      )}
+
+      {editarConsultas && (
+        <div className={style.modalOverlay} onClick={fecharModal}>
+          <div className={style.modal} onClick={e => e.stopPropagation()}>
+            <h2>Editar Consultas</h2>
             <button onClick={fecharModal} className={style.fechar}>Fechar</button>
           </div>
         </div>
